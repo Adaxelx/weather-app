a01d578e3361153c83cbe2404d8b7c1c1ad2347c
Adrian Mostowski <adaxelx@o2.pl>
2019-10-12 15:48:49 +0200

    Updates

---
+++ precache-manifest.9d0698de9527a4f4e07f0d13a9c7a1de.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a07ba2b590dcf3203c730c2de4f2840",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "a494f1c56a920af5abdd",
+    "url": "/weather-app/static/js/2.22e8f368.chunk.js"
+  },
+  {
+    "revision": "eddc9af8ce2b5d70594f",
+    "url": "/weather-app/static/js/main.dfd8fc6a.chunk.js"
+  },
+  {
+    "revision": "52387862e54d9be38456",
+    "url": "/weather-app/static/js/runtime~main.68dbe4ab.js"
+  }
+]);
