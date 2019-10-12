636d707b4d6826be11e4853a5bfe9cd25f3555fc
Adrian Mostowski <adaxelx@o2.pl>
2019-10-12 14:15:49 +0200

    add working fetch

--- src/components/organisms/ListOfCities/ListOfCities.js
+++ src/components/organisms/ListOfCities/ListOfCities.js
@@ -1,13 +1,10 @@
 import React from 'react';
-
 /* eslint-disable no-undef */
 
-const ListOfCities = ({ city }) => {
-  return (
-    <>
-      <p>{city}</p>
-    </>
-  );
+const ListOfCities = () => {
+  const wrongMessage =
+    'Coś poszło nie tak. Sprawdź czy dobrze wpisałeś nazwę miasta(Zagraniczne miasta nalezy wpisać po angielsku.) ';
+  return <>{wrongMessage}</>;
 };
 
 export default ListOfCities;
