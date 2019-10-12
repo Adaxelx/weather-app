51287d4167e6e5b85f59f4433de089adb5ab3ad9
Adrian Mostowski <adaxelx@o2.pl>
2019-10-12 15:43:03 +0200

    repair4

--- src/components/atoms/Title.js
+++ src/components/atoms/Title.js
@@ -10,5 +10,6 @@ export const Title = styled.h1`
       font-size: ${({ theme }) => theme.fontSize.xl};
       text-transform: uppercase;
       margin-top: 20px;
+      margin-bottom: 0;
     `}
 `;
